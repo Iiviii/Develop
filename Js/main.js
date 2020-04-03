19e11943dc3486981e4eaa5c39a87bcab93b68c5
Iiviii <mariamzirakashvili0@gmail.com>
2020-04-03 21:59:19 +0400

    add Js file

---
+++ Js/main.js
@@ -0,0 +1,10 @@
+var right = document.getElementById('right');
+var left = document.getElementById('left');
+
+right.addEventListener('click', function(){
+    console.log("right clicked")
+});
+
+left.addEventListener('click', function(){
+    console.log("left clicked")
+})
